a95265f193c47558d354df30aa10a39ed46e0cab
inesbl2 <156537484+inesbl2@users.noreply.github.com>
2024-05-25 15:29:07 +0200

    Final version of main

--- main.py
+++ main.py
@@ -8,9 +8,12 @@ from  praw.models.comment_forest import CommentForest
 from google.cloud import bigquery
 from google.cloud.exceptions import NotFound
 from flask import Request, jsonify
-
+import time
+from datetime import datetime as date
+import logging
 
 user_agent = "Scrapper 1.0 by /u/Reasonable-Fold-8491"
+
 
 client_id = os.environ["client_id"]
 client_secret = os.environ["client_secret"]
@@ -25,6 +28,7 @@ def get_data(subreddit: str, num_results: int):
     headlines = []
     for submission in reddit.subreddit(subreddit).hot(limit=num_results):
         headlines.append(submission)
+    print(headlines)
     return headlines
 
 def extract_all_replies(comment: Comment, acc: List[str]) -> List[str]:
@@ -50,34 +54,44 @@ def get_all_comments(post: Submission) -> List[str]:
         extract_all_replies(comment, all_comments)
     return all_comments
 
+
+
 def data_acquisition(request):
-    
-    if request.method != "POST":
-        return jsonify({"status": "error", "message": "Invalid request method. Please use POST."}), 405
-    
+
     request_json = request.get_json()
     subreddits = request_json["subreddits"]
     num_posts = request_json["num_posts"]
-    
+
     all_posts = []
     for subreddit in subreddits:
+        try:
+            submissions = get_data(subreddit, num_posts)
+        except Exception as e:
+            logging.error(f"Error retrieving data from {subreddit}: {e}")
+            continue
 
-        submissions = get_data(subreddit, num_posts)
-        
         for submission in submissions:
-            post = {
-                "reddit": subreddit,
-                "id": submission.id,
-                "title": submission.title,
-                "author_id": submission.author.id,
-                "comments": get_all_comments(submission),
-                "url": submission.url
-            }
-            print(f"The post {submission.title} has been retrieved")
-            all_posts.append(post)
+            try:
+                post = {
+                    "reddit": subreddit,
+                    "id": submission.id,
+                    "title": submission.title,
+                    "author_id": submission.author.id,
+                    "comments": get_all_comments(submission),
+                    "url": submission.url, 
+                    "date" : date.now().isoformat()
+                }
+                print(f"The post {submission.title} has been retrieved")
+                all_posts.append(post)
+            except Exception as e:
+                logging.error(f"Error processing submission {submission.id}: {e}")
+
+            time.sleep(1)  
+
+    
     write_to_db(all_posts)
     return jsonify({"status": "success", "message": "Data acquired and written to BigQuery"}), 200
-
+    
             
         
 def write_to_db(posts: List[Dict]):
@@ -91,9 +105,9 @@ def write_to_db(posts: List[Dict]):
     bigquery.SchemaField("title", "STRING", mode="REQUIRED"),
     bigquery.SchemaField("author_id", "STRING", mode="REQUIRED"),
     bigquery.SchemaField("comments",  "STRING", mode="REPEATED"),
-    bigquery.SchemaField("url", "STRING", mode="REQUIRED")]
-    # Add an ingestion date field so the sentiment analyser service can remove the duplicates and 
-    # keep the row with the most recent date
+    bigquery.SchemaField("url", "STRING", mode="REQUIRED"),
+    bigquery.SchemaField("date", "TIMESTAMP", mode="REQUIRED")]
+   
     table = bigquery.Table(full_table_id, schema=schema)
 
     try:
@@ -108,3 +122,5 @@ def write_to_db(posts: List[Dict]):
         print("New rows have been added.")
     else:
         print("Errors occurred while inserting rows: ", errors)
+    
+    
